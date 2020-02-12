6404ae4166ea64aa3036c5f4d306399f89fd37c5
DEED1T <60522466+DEED1T@users.noreply.github.com>
2020-02-12 12:08:42 +0100

    la méthode pour judicael

--- src/Modele.java
+++ src/Modele.java
@@ -2,7 +2,22 @@ import java.awt.Color;
 
 public class Modele {
 	public static Color[] COULEURS = {Color.yellow, Color.blue,Color.magenta,Color.red,Color.orange, Color.white,Color.black};
-	public int TENTATIVES = 10;
-	public int DIFFICULTE = 4;
+	public static int TENTATIVES = 10;
+	public static int DIFFICULTE = 4;
+	public static enum Etat {EN_COURS, GAGNE, PERDU};
+	
+	private Etat etat;
+	private Rangee combinaison = new Rangee();
+	private Rangee[] propositions = new Rangee[TENTATIVES];
+	private int tentatives;
+	
+	public void archiverProp() {
+		
+	}
+	
+	public Rangee[] getProp() {
+		return propositions;
+	}
+	
 	
 }
