e242d0ddfcb0a999a004478bd2a2ca1e99b58dd1
Weedris <jud.thivet@gmail.com>
2020-02-12 10:59:11 +0100

    Ajout de la vue dans les sources

---
+++ src/Vue.java
@@ -0,0 +1,15 @@
+import java.awt.Frame;
+
+public class Vue extends Frame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5769119333854024922L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
