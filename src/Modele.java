0839d79f8b2a0084a561271f5f831a1c7f662c60
DEED1T <60522466+DEED1T@users.noreply.github.com>
2020-02-12 10:46:31 +0100

    fichier src

---
+++ src/Modele.java
@@ -0,0 +1,8 @@
+import java.awt.Color;
+
+public class Modele {
+	public static Color[] COULEURS = {Color.yellow, Color.blue,Color.magenta,Color.red,Color.orange, Color.white,Color.black};
+	public int TENTATIVES = 10;
+	public int DIFFICULTE = 4;
+	
+}
