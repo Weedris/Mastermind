f405362986d06b68acd59f878560b23519c2b10a
Hamdi Lazaar <hlazaar@pcalb-mm0000.univ-jfc.fr>
2020-03-04 11:26:25 +0100

    rajout des methodes du modèles sans etre tester

--- src/Modele.java
+++ src/Modele.java
@@ -6,18 +6,60 @@ public class Modele {
 	public static int DIFFICULTE = 4;
 	public static enum Etat {EN_COURS, GAGNE, PERDU};
 	
-	private Etat etat;
+	private Etat etat = Etat.EN_COURS;
 	private Rangee combinaison = new Rangee();
 	private Rangee[] propositions = new Rangee[TENTATIVES];
-	private int tentatives;
+	private Rangee prop_actuel;
+	private int tentatives = 0;
 	
 	public void archiverProp() {
-		
+		propositions[tentatives] = prop_actuel;
+		tentatives +=1;
 	}
 	
-	public Rangee[] getProp() {
+	public Rangee[] get_prop() {
 		return propositions;
 	}
 	
+	public void prop_verification() {
+		if(prop_actuel.indice_jeton+1 == combinaison.jetons.length) {
+			for(int i = 0; i<combinaison.jetons.length; i++) {
+				for(int j = 0; j<combinaison.jetons.length; j++) {
+					if(prop_actuel.jetons[j].equals(combinaison.jetons[i]) && i == j){
+						prop_actuel.blancs +=1;
+					}
+					else if(prop_actuel.jetons[j].equals(combinaison.jetons[i])) {
+						prop_actuel.noirs +=1;
+					}
+				}
+			}
+		}
+	}
+
+	public void new_prop() {
+		prop_actuel.indice_jeton = 0;
+		prop_actuel.blancs = 0;
+		prop_actuel.noirs = 0;
+		
+	}
+	
+	public void complete_prop(Color c, int i) {
+		if(i<prop_actuel.indice_jeton) {
+			prop_actuel.jetons[i] = c;
+			prop_actuel.indice_jeton +=1;
+		}
+	}
+	
+	public boolean win() {
+		if(prop_actuel.blancs == DIFFICULTE) {
+			etat = Etat.GAGNE;
+		}
+		else if(tentatives>=TENTATIVES) {
+			etat = Etat.PERDU;
+		}
+		
+		return etat == Etat.GAGNE;
+	}
 	
 }
+
